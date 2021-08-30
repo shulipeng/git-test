ebf2233f2c0edaf22a03b061dfe478626a16cfc6
shulipeng <pengxianyang1996@163.com>
2021-08-30 23:22:17 +0800

    Update GitClass.java

--- src/main/java/com/shulipeng/git/GitClass.java
+++ src/main/java/com/shulipeng/git/GitClass.java
@@ -4,16 +4,5 @@ public class GitClass {
 
     public static void main(String[] args) {
         System.out.println("hello git ");
-        System.out.println("hello git 3 ");
-        System.out.println("hot fix update");
-        System.out.println("hot fix update 2222");
-        System.out.println("master update");
-        System.out.println("master update");
-        System.out.println("master update");
-        System.out.println("master update");
-        System.out.println("master update");
-        System.out.println("master update");
-        System.out.println("master update");
-        System.out.println("master update");
     }
 }
