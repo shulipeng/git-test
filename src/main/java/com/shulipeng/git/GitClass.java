0f3dd8ff669a73805a60edcf38dc53b4de5f96a1
xiany.peng <xiany.peng@hniar.com>
2021-08-30 22:55:22 +0800

    hotfix update

--- src/main/java/com/shulipeng/git/GitClass.java
+++ src/main/java/com/shulipeng/git/GitClass.java
@@ -5,5 +5,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello git ");
         System.out.println("hello git 3 ");
+        System.out.println("hot fix update");
     }
 }
